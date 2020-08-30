96ce4535b59f9f9a0542c364515845775c616ed3
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2020-08-31 01:47:33 +0200

    Add traffic light animation

--- app/src/main/java/pl/gdgwroclaw/workshop/styles/TrafficLightView.kt
+++ app/src/main/java/pl/gdgwroclaw/workshop/styles/TrafficLightView.kt
@@ -1,11 +1,13 @@
 package pl.gdgwroclaw.workshop.styles
 
 import android.content.Context
+import android.graphics.Canvas
 import android.graphics.Color
 import android.util.AttributeSet
 import android.view.View
 import androidx.annotation.ColorInt
 import androidx.core.content.withStyledAttributes
+import androidx.core.view.postDelayed
 import kotlin.properties.Delegates
 
 class TrafficLightView @JvmOverloads constructor(
@@ -16,11 +18,38 @@ class TrafficLightView @JvmOverloads constructor(
 ) : View(context, attrs, defStyleAttr, defStyleRes) {
     @get:ColorInt
     private var color by Delegates.notNull<Int>()
+    private var isAnimating = false
 
     init {
-        context.withStyledAttributes(attrs, R.styleable.TrafficLightView, defStyleAttr, defStyleRes) {
+        context.withStyledAttributes(
+            attrs,
+            R.styleable.TrafficLightView,
+            defStyleAttr,
+            defStyleRes
+        ) {
             color = getColor(R.styleable.TrafficLightView_color, Color.CYAN)
         }
-        setBackgroundColor(color)
+        setOnClickListener {
+            color = Color.YELLOW
+            invalidate()
+        }
+        setOnLongClickListener {
+            isAnimating = true
+            changeColor()
+            true
+        }
+    }
+
+    private fun changeColor() {
+        color = when (color) {
+            Color.RED -> Color.GREEN
+            else -> Color.RED
+        }
+        invalidate()
+        postDelayed(500, ::changeColor)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        canvas.drawColor(color)
     }
 }
