8c9c3bb00d22fa327fca71af14699053917b4c7d
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2020-08-31 01:17:15 +0200

    Add simple TrafficLightView

---
+++ app/src/main/java/pl/gdgwroclaw/workshop/styles/TrafficLightView.kt
@@ -0,0 +1,27 @@
+package pl.gdgwroclaw.workshop.styles
+
+import android.content.Context
+import android.graphics.Color
+import android.util.AttributeSet
+import android.view.View
+import androidx.annotation.ColorInt
+import androidx.core.content.withStyledAttributes
+import kotlin.properties.Delegates
+
+class TrafficLightView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0
+) : View(context, attrs, defStyleAttr, defStyleRes) {
+
+    @get:ColorInt
+    private var color by Delegates.notNull<Int>()
+
+    init {
+        context.withStyledAttributes(attrs, R.styleable.TrafficLightView, defStyleAttr, defStyleRes) {
+            color = getColor(R.styleable.TrafficLightView_color, Color.CYAN)
+        }
+        setBackgroundColor(color)
+    }
+}
